3c3459f238823d11befb3f716ac62e6f11fd5f6e
Zuckuu <entbit12@gmail.com>
2022-07-17 16:44:36 -0500

    adding fontawesome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Features from "../components/Featured";
 import CheapestBooks from "../components/CheapestBooks";
 import Explore from "../components/Explore";
 
+
 const Home = () => {
   return (
     <>
